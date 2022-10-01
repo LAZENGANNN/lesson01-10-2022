74a184c7b31e533d753690622a77b6674525b85b
LAZENGANNN <113790875+LAZENGANNN@users.noreply.github.com>
2022-10-01 19:26:34 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,186 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+
+	//if7.
+
+//int a;
+//int b;
+//
+//cin >> a >> b;
+//if (a > b)
+//{
+//	cout << 2;
+//}
+//else if (a < b)
+//{
+//	cout << 1;
+//}
+//else if (a == b)
+//{
+//	cout << "=";
+//}
+//
+//
+
+	//if8
+	/*int a;
+	int b;
+	cin >> a >> b;
+	if (a > b)
+	{
+		cout << a << " " << b;
+	}
+	else if (a < b)
+	{
+		cout << b << " " << a;
+	}
+	else if (a == b)
+	{
+		cout << "=";
+	}*/
+
+	//if9
+	/*double a, b;
+	cin >> a >> b;
+	if (a > b) {
+		double t = a;
+		a = b;
+		b = t;
+	}
+	cout << a << " " << b;*/
+
+	//if10
+	/*int a;
+int b;
+cin >> a >> b;
+if (a != b)
+{
+	int s = a + b;
+	int b = s;
+	int a = s;
+	
+}
+else 
+{
+	a = b = 0;
+	
+}
+cout << a << " " << b;*/
+
+	//if12
+	/*int a;
+int b;
+int c;
+cin >> a >> b >> c;
+if (a < b&& a < c)
+{
+	cout << a;
+}
+if (b < a&& b < c)
+{
+	cout << b;
+}
+if (c < a&& c < b)
+{
+	cout << c;
+}*/
+
+//if13
+//int a;
+//int b;
+//int c;
+//cin >> a >> b >> c;
+//if (a > b&& a < c)
+//{
+//	cout << a;
+//}
+//else if (b > a&& b < c)
+//{
+//	cout << b;
+//}
+//else if (c > a&& c < b)
+//{
+//	cout << c;
+//}
+
+  //if24
+//double fx;
+//double x;
+//cin >> x;
+//if (x > 0)
+//{
+//	fx = 2 * sin(x);
+//}
+//else if (x <= 0) 
+//{
+//	fx = 6 - x;
+//}
+//cout << fx;
+
+//if 25
+//double fx;
+//double x;
+//cin >> x;
+//if (x < -2 || x > 2 )
+//{
+//	fx = 2 * x;
+//}
+//else 
+//{
+//	fx = -3 * x;
+//}
+//cout << fx;
+
+//if 26
+//double fx;
+//double x;
+//cin >> x;
+//if (x <= 0)
+//{
+//	fx = x * (-1);
+//}
+//else if ( 0 < x && x < 2 ) 
+//{
+//	fx = x * x;
+//}
+//else if (x>= 2)
+//{
+//	fx = 4;
+//}
+//cout << fx;
+
+//if 29
+//int a;
+//cin >> a;
+//int d = a % 2;
+//if (a == 0) {
+//	cout << "нулевое";
+//}
+//else if (a > 0) {
+//
+//	if ( d == 0) { 
+//		cout << "чётное положительное"; 
+//	}
+//	else {
+//		cout << "нечётное положительное";
+//	}
+//}
+//else if (a < 0) {
+//	if (d == 0) {
+//		cout << "чётное отрицательное";
+//	}
+//	else {
+//		cout << "нечётное отрицательное";
+//	}
+//}
+
+
+
+
+
+
+	
+}
